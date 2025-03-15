dbcb7b04de3cfd5e67f18bd80efe9bf59774a924
Cameron <cameron.cassidy@gmail.com>
2025-03-15 18:02:58 +0000

    added temp water tile grid

---
+++ Assets/Scripts/Environment/WaterMovementWithWind.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class WaterMovementWithWind : MonoBehaviour
+{
+    private const float Boundary = 10f; //one tile size is 10 units
+    public float windInfluence = 0.5f; //how much the wind affects the water
+
+    void Update()
+    {
+        Vector3 windDir = WindMgr.Instance.windDir;
+        float windStrength = WindMgr.Instance.windStrength;
+
+        Vector3 movement = windDir * windStrength * windInfluence * Time.deltaTime;
+        transform.position += movement;
+
+        Vector3 pos = transform.position;
+
+        if (pos.x >= Boundary) pos.x -= Boundary;
+        else if (pos.x <= -Boundary) pos.x += Boundary;
+
+        if (pos.z >= Boundary) pos.z -= Boundary;
+        else if (pos.z <= -Boundary) pos.z += Boundary;
+
+        transform.position = pos;
+    }
+}
