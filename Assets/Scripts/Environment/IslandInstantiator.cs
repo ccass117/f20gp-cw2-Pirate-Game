354c589ffe7ff1cde83634b7c9fb1727311edbe7
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-03-18 10:36:07 +0000

    added island instantiator

--- Assets/Scripts/Environment/IslandInstantiator.cs
+++ Assets/Scripts/Environment/IslandInstantiator.cs
@@ -3,62 +3,67 @@ using System.Collections.Generic;
 
 public class IslandInstantiator : MonoBehaviour
 {
-    public GameObject[] go;
+    public GameObject[] tileSet;
+    private Transform selfT;
+    public int maxWidth = 5;
+    public int maxHeight = 5;
+    public int mass = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //generate island
+        // get self transform component
+        selfT = GetComponent<Transform>();
 
-        int h = 10;
-        int w = 10;
-        List<List<(int, bool, int)>> a = Islands.genIsland(w, h, 30, 5f);
-        for (int y = 0; y < h + 1; y++)
+        //generate island
+        List<List<(int, bool, int)>> a = Islands.genIsland(maxWidth, maxHeight, mass, 5f);
+
+        // instantiate the prefabs
+        for (int y = 0; y < maxHeight + 1; y++)
         {
-            for (int x = 0; x < w + 1; x++)
+            for (int x = 0; x < maxWidth + 1; x++)
             {
                 (int, bool, int) cur = a[y][x];
                 int id = cur.Item1;
                 bool flip = cur.Item2;
                 int rotation = cur.Item3;
+
+                // skip empty ones (remove if testing)
                 if (id == 0) continue;
 
-                GameObject item = go[0];
+                // linear search for prefabs
+                GameObject item = tileSet[0];
                 bool found = false;
 
-                for (int i = 0; i < go.Length; i++)
+                for (int i = 0; i < tileSet.Length; i++)
                 {
-                    if (int.Parse(go[i].name) == id)
+                    if (int.Parse(tileSet[i].name) == id)
                     {
-                        item = go[i];
+                        item = tileSet[i];
                         found = true;
                     }
                 }
 
+                // if tile not found, panic
                 if (!found)
                 {
                     Debug.Log("not found :");
                     Debug.Log(id);
                     continue;
                 }
+
+                // actually instantiate the right prefab
                 GameObject b = Instantiate(item);
-                b.transform.position = new Vector3(x * 6, 0, y * -6);
-                Debug.Log(b.transform.position);
+
+                // change positions rotation and flip
+                b.transform.position = new Vector3(selfT.position.x + x * 6 - (maxWidth * 3), 0, selfT.position.z + y * -6 + (maxHeight * 3));
                 if (flip)
                 {
                     b.transform.localScale = new Vector3(-1, 1, 1);
                 }
                 b.transform.rotation = Quaternion.Euler(0, rotation * 90, 0);
 
-
             }
-
         }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
