f38dafb488fe13d4274e576a2353f55ec4218fa1
Cameron Cassidy <cameron.cassidy@gmail.com>
2025-03-26 18:56:12 +0000

    added fireball and meteormanager

---
+++ Assets/Scripts/Environment/meteorRock.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class meteorRock : MonoBehaviour
+{
+    AudioSource splash;
+    bool splashed = false;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        splash = GetComponent<AudioSource>();
+
+    }
+    private void Update()
+    {
+        if (transform.position.y <= 0 && !splashed)
+        {
+            splashed = !splashed;
+            splash.Play();
+        }
+        
+        if (transform.position.y <= -50)
+        {
+
+            Destroy(transform.parent.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the other object has the "fireball shadow" tag
+        if (other.CompareTag("fireball shadow"))
+        {
+            // Destroy the object with the "fireball shadow" tag
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the other object is tagged as "player"
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            // Example action: Destroy the object
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
