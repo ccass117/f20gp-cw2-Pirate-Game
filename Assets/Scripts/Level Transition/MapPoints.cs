378fba3dc678f6c48e6088c52dc8c2d15673223a
taniahenderson <39859763+taniahenderson@users.noreply.github.com>
2025-03-31 14:36:36 +0100

    adjusted sprites for a better looking map

--- Assets/Scripts/Level Transition/MapPoints.cs
+++ Assets/Scripts/Level Transition/MapPoints.cs
@@ -5,14 +5,6 @@ public class MapPoints : MonoBehaviour
 {
     [SerializeField] public List<Transform> points = new List<Transform>();
 
-    private void Start()
-    {
-        // to handle any number of points on the map
-        foreach (Transform point in transform)
-        {
-            points.Add(point);
-        }
-    }
     public Transform GetPoint(int i)
     {
         if (i < points.Count)
@@ -22,26 +14,4 @@ public class MapPoints : MonoBehaviour
         // if going out of bounds then return last point to prevent error
         return points[points.Count - 1];
     }
-
-
-    /*
-    public Vector3 GetNextPoint(int i)
-    {
-        // returns correctly if index is within bounds
-        if (i < points.Count - 1)
-        {
-            return points[i].position;
-        }
-        // if going out of bounds then return last point to prevent error
-        return points[points.Count - 1].position; 
-    }
-    public Transform GetThisPoint(int i)
-    {
-        if (i >= 0 && i < points.Count)
-        {
-            return points[i];
-        }
-        return null;
-    }
-    */
 }
