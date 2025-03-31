9a39a8dc335668d42b6db2f89f71e5bd828dde6a
taniahenderson <39859763+taniahenderson@users.noreply.github.com>
2025-03-31 20:10:40 +0100

    handle diff screen sizes/level change logic/main menu surprise :]

--- Assets/Scripts/UI/UIAnimator.cs
+++ Assets/Scripts/UI/UIAnimator.cs
@@ -4,20 +4,18 @@ using UnityEngine.UI;
 
 public class UIAnimator : MonoBehaviour
 {
-    private const float moveTime = 1.8f; // duration of animation
-
     // brings UI elements into view
     public static void AnimateUI(RectTransform titleText, RectTransform[] buttons, float width)
     {
         titleText.anchoredPosition = new Vector2(-width, titleText.anchoredPosition.y);
-        titleText.DOAnchorPosX(0, moveTime)
+        titleText.DOAnchorPosX(0, 2.2f)
             .SetEase(Ease.OutBack);
 
         // to handle any number of buttons
         foreach (RectTransform button in buttons)
         {
             button.anchoredPosition = new Vector2(width, button.anchoredPosition.y);
-            button.DOAnchorPosX(0, moveTime)
+            button.DOAnchorPosX(0, 2.2f)
                 .SetEase(Ease.OutBack)
                 .OnComplete(() =>
                 {
@@ -30,12 +28,12 @@ public class UIAnimator : MonoBehaviour
     // takes UI elements out of view
     public static void AnimateUIOut(RectTransform title, RectTransform[] buttons, float width)
     {
-        title.DOAnchorPosX(-width, moveTime)
+        title.DOAnchorPosX(-width, 1.8f)
             .SetEase(Ease.InBack);
 
         foreach (RectTransform button in buttons)
         {
-            button.DOAnchorPosX(width, moveTime)
+            button.DOAnchorPosX(width, 1.8f)
                 .SetEase(Ease.InBack);
         }
     }
