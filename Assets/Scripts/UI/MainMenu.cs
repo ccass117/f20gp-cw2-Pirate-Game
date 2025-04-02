4f6738aa993252cce995fb42076b81ef30391cff
Tania <39859763+taniahenderson@users.noreply.github.com>
2025-04-03 00:01:46 +0100

    Update MainMenu.cs

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,54 +4,73 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Scripts")]
+    [SerializeField] private LevelLoader levelLoader;
+    [SerializeField] private Credits credits;
+
+    [Header("Main")]
     [SerializeField] private RectTransform titleText;
+
     [SerializeField] private RectTransform startBtn; // UI element
-    [SerializeField] private RectTransform quitBtn;
-
-    [SerializeField] private AudioSource clickStartSound; // cannonball firing sound for when start is clicked
-    [SerializeField] private Animator cameraAnim;
-
     [SerializeField] private Button startButton; // control button interactability
+
+    [SerializeField] private RectTransform creditsBtn;
+    [SerializeField] private Button creditsButton;
+
+    [SerializeField] private RectTransform quitBtn;
     [SerializeField] private Button quitButton;
 
-    [SerializeField] private LevelLoader levelLoader;
+    [Header("Misc")]
+    [SerializeField] private AudioSource audioSource; // cannonball firing sound for when start is clicked
+    [SerializeField] private Animator anim;
 
     private RectTransform canvasSize;
     private float canvasWidth;
-    private float moveTime = 1.8f; // how long it takes for the UI elements to move
 
     private void Start()
     {
         canvasSize = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
         canvasWidth = canvasSize.rect.width;
 
-        // set these to non-interactable at first
+        // set these to non-interactable so player can't click them while animation is playing
         startButton.interactable = false;
         quitButton.interactable = false;
 
-        cameraAnim.SetTrigger("MainMenu");
-        DOVirtual.DelayedCall(2f, AnimUIIn);
+        anim.SetTrigger("MainMenu");
+        DOVirtual.DelayedCall(2f, AnimUIMainIn);
     }
 
-    void AnimUIIn()
+    // DOTween transition that brings in the title and buttons
+    public void AnimUIMainIn()
     {
-        UIAnimator.AnimateUI(titleText, new RectTransform[] { startBtn, quitBtn }, canvasWidth);
+        UIAnimator.AnimateUIIn(titleText, new RectTransform[] { startBtn, creditsBtn, quitBtn }, canvasWidth);
     }
-
 
     public void Play()
     {
-        clickStartSound.Play();
-        UIAnimator.AnimateUIOut(titleText, new RectTransform[] { startBtn, quitBtn }, canvasWidth);
+        audioSource.Play();
+        UIAnimator.AnimateUIOut(titleText, new RectTransform[] { startBtn, creditsBtn, quitBtn }, canvasWidth);
 
-        // give time for anims to finish before loading next scene
-        DOVirtual.DelayedCall(moveTime + 0.5f, () =>
+        // give time for transition to finish before loading next scene
+        DOVirtual.DelayedCall(1.8f + 0.5f, () =>
         {
             DOTween.KillAll();
             levelLoader.LoadLevel("GoldShop");
         });
     }
+    
+    // transition to credits
+    public void Credits()
+    {
+        UIAnimator.AnimateUIOut(titleText, new RectTransform[] { startBtn, creditsBtn, quitBtn }, canvasWidth);
+        DOVirtual.DelayedCall(1.5f, () =>
+        {
+            anim.SetTrigger("ToCredits");
+            credits.RollCredits();
+        });
+    }
 
+    // closes the game (build version only)
     public void Quit()
     {
         Application.Quit();
